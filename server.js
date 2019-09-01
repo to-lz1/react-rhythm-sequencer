6d84f1c5f6d9f4d586ca4a489a15744d7cd20514
to-lz1 <m.toriyama000@gmail.com>
2019-09-01 10:19:22 +0900

    rewrite express server

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
-var fs = require('fs');
-var path = require('path');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-
-var COMMENTS_FILE = path.join(__dirname, 'comments.json');
-
-app.set('port', (process.env.PORT || 3000));
+const path = require('path');
+const express = require('express');
+const app = express();
 
 app.use('/', express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
-// Additional middleware which will set headers that we need on each request.
 app.use(function(req, res, next) {
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Server started: http://localhost:' + app.get('port') + '/');
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Server started: http://localhost:' + port + '/');
 });
